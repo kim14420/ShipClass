649c8a48349b3233251784c26364e1e7dae894d0
kim14420 <101626094+kim14420@users.noreply.github.com>
2022-06-13 22:03:09 +0900

    Update ShipTest.java

--- ShipTest.java
+++ ShipTest.java
@@ -1,6 +1,7 @@
 package exam10;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class ShipTest {
@@ -8,53 +9,63 @@ public class ShipTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	HashMap <String, Object>map = new HashMap<String, Object>();
-	
-	Boat boat1 = new Boat();
-	boat1.setShipName("Boat01");
-	boat1.setFuelTank(500);
-	
-	map.put("1", boat1);
-	
-	Cruise cruise1 = new Cruise();
-	cruise1.setShipName("Cruise01");
-	cruise1.setFuelTank(1000);
-	
-	map.put("2", cruise1);
-	
+		HashMap <String, Object>map = new HashMap<String, Object>();
+		Scanner in = new Scanner(System.in);
+		
+		
+		Boat boat1 = new Boat();
+		boat1.setShipName("Boat01");
+		boat1.setFuelTank(500);
+		
+		map.put("1", boat1);
+		
+		Cruise cruise1 = new Cruise();
+		cruise1.setShipName("Cruise01");
+		cruise1.setFuelTank(1000);
+		
+		map.put("2", cruise1);
+		
 	System.out.println("shipName" + "\tfuelTank");
 	System.out.println("--------------------------");
-	
-	for (int i = 1; i < 3; i++) {
-		System.out.println(map.get(i+""));
-	}
-	
+		
+		for (int i = 1; i < 3; i++) {
+			System.out.println(map.get(i+""));
+		}
+		
 	System.out.println();
-	System.out.println("10운항");
-	System.out.println("shipName" + "\tfuelTank");
-	System.out.println("--------------------------");
-
-
-	for (int i = 1; i < 3; i++) {
-		((Ship)map.get(i+"")).sail(10);;
-	}
+	System.out.print("운항 할 거리를 입력하시오: ");
+		int sail = 0;
+		sail = in.nextInt();
 	
-	for (int i = 1; i < 3; i++) {
-		System.out.println(map.get(i+""));
-	}
-	
+		
+		for (int i = 1; i < 3; i++) {
+			((Ship)map.get(i+"")).sail(sail);;
+		}
+		
+		
 	System.out.println();
-	System.out.println("5주유");
 	System.out.println("shipName" + "\tfuelTank");
 	System.out.println("--------------------------");
-
-
-	for (int i = 1; i < 3; i++) {
-		((Ship)map.get(i+"")).refuel(5);
-	}
+		
+		for (int i = 1; i < 3; i++) {
+			System.out.println(map.get(i+""));
+		}
+		
+	System.out.println();
+	System.out.print("주유할 양을 입력하시오: ");
+		int fuel = 0;
+		fuel = in.nextInt();
 	
-	for (int i = 1; i < 3; i++) {
-		System.out.println(map.get(i+""));
-	}
+		for (int i = 1; i < 3; i++) {
+			((Ship)map.get(i+"")).refuel(fuel);
+		}
+		
+	System.out.println();
+	System.out.println("shipName" + "\tfuelTank");
+	System.out.println("--------------------------");
+		
+		for (int i = 1; i < 3; i++) {
+			System.out.println(map.get(i+""));
+		}
 	}
 }
